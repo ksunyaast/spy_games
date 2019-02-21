7b6775a44900557b8cdfcd06c40f9a6fdd1569cf
ksunyaast <ksunyaast@mail.ru>
2019-02-21 22:13:57 +0700

    rewrote the code

--- spy_games.py
+++ spy_games.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 import requests
 import json
 import time
@@ -11,48 +10,45 @@ if __name__ == '__main__':
         'v': '5.92',
         'access_token': token,
     }
-    try:
-        response = requests.get('https://api.vk.com/method/users.get?user_ids=' + user, params)
-        print('-')
-        user_ids = response.json()
-        user_id = user_ids['response'][0]['id']
 
-        response = requests.get('https://api.vk.com/method/friends.get?user_id=' + str(user_id), params)
-        print('-')
-        user_friends = response.json()
-        user_friends_list = user_friends['response']['items']
-
-        response = requests.get('https://api.vk.com/method/groups.get?user_id=' + str(user_id), params)
-        print('-')
-        user_groups = response.json()
-        user_groups_list = user_groups['response']['items']
-
-        no_match_groups_list = list()
-        for group in user_groups_list:
-            response = requests.get('https://api.vk.com/method/groups.isMember?group_id=' + str(group) + '&user_ids=' + str(user_friends_list), params)
+    def get_data(url, params):
+        while True:
+            response = requests.get(url, params=params).json()
             print('-')
-            matches = response.json()
-            members = 0
-            for match in matches['response']:
-                members += match['member']
-            if members == 0:
-                no_match_groups_list.append(group)
-            else:
-                pass
+            try:
+                return response['response']
+            except KeyError:
+                print('request error: ', response)
+                time.sleep(1)
 
-        no_match_groups_info_list = list()
-        for no_match_group in no_match_groups_list:
-            response = requests.get('https://api.vk.com/method/groups.getById?group_ids=' + str(no_match_group) +
-                                '&fields=members_count', params)
-            print('-')
-            groups_info = response.json()
-            no_match_group_dict = dict()
-            no_match_group_dict['name'] = groups_info['response'][0]['name']
-            no_match_group_dict['gid'] = str(groups_info['response'][0]['id'])
-            no_match_group_dict['members_count'] = groups_info['response'][0]['members_count']
-            no_match_groups_info_list.append(no_match_group_dict)
-    except KeyError:
-        time.sleep(2)
+    url = 'https://api.vk.com/method/users.get?user_ids=' + user
+    user_id = get_data(url, params)[0]['id']
+
+    url = 'https://api.vk.com/method/friends.get?user_id=' + str(user_id)
+    user_friends_list = get_data(url, params)['items']
+
+    url = 'https://api.vk.com/method/groups.get?user_id=' + str(user_id)
+    user_groups_list = get_data(url, params)['items']
+
+    no_match_groups_list = list()
+    for group in user_groups_list:
+        url = 'https://api.vk.com/method/groups.isMember?group_id=' + str(group) + '&user_ids=' + str(user_friends_list)
+        members = 0
+        for match in get_data(url, params):
+            members += match['member']
+        if members == 0:
+            no_match_groups_list.append(group)
+        else:
+            pass
+
+    no_match_groups_info_list = list()
+    for no_match_group in no_match_groups_list:
+        url = 'https://api.vk.com/method/groups.getById?group_ids=' + str(no_match_group) + '&fields=members_count'
+        no_match_group_dict = dict()
+        no_match_group_dict['name'] = get_data(url, params)[0]['name']
+        no_match_group_dict['gid'] = str(get_data(url, params)[0]['id'])
+        no_match_group_dict['members_count'] = get_data(url, params)[0]['members_count']
+        no_match_groups_info_list.append(no_match_group_dict)
 
     with open('no_match_groups.json', 'w', encoding='utf-8') as f:
         json.dump(no_match_groups_info_list, f, ensure_ascii=False, indent=2)
